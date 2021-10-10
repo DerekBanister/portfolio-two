922c23b37ebb73d8a634997e3d50518c87d975d3
Derek Banister <ptownderek@yahoo.com>
2021-10-09 22:35:52 -0700

    Styling changes to button, colors, and links

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var jumbo = $(".jumbotron")
 
-$(".btn").on("click", function(event){
+$(".meBtn").on("click", function(event){
     event.preventDefault();
-    $(".btn").hide();
+    $(".btnHide").hide();
     appendAboutMe();
 
 })
@@ -10,7 +10,7 @@ $(".btn").on("click", function(event){
 function appendAboutMe(){
 
     var aboutMe = $("<div>")
-    var myText = $("<p>").text("Thanks for visiting my page! I am 26 years old, born and raised in Pleasanton, CA. I am a former law enforcement student and Contra Costa County Police Academy graduate switching careers into the field of web development. I am incredibly excited to begin this new journey in my life and acquire new skills to reach my goals. I am a motivated problem solver that enjoys spending time with my dog Raider and my wife Katrina.")
+    var myText = $("<p>").text("Thanks for visiting my page! I am 26 years old, born and raised in Pleasanton, CA. I am a former law enforcement student and Contra Costa County Police Academy graduate switching careers into the field of web development. I am incredibly excited to begin this new journey in my life and acquire new skills to reach my goals. The technologies I have learned so far include HTML, CSS, Javascript, Jquery, and Bootstrap. I will add many more libraries and frameworks to my arsenal and absolutely love learning new technologies. I enjoy playing sports, watching movies, video games, and spending time with my dog Raider and my wife Katrina.")
     var myPic = $("<img>").attr("src", "images/me.jpg")
     myPic.attr("style", "width: 400px")
     aboutMe.append(myText, myPic)
